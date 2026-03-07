a0be3de822ad3dd97127f639806facf13408fe09
Golla Sreenivas Yadav -X (golyadav - CAPGEMINI TECHNOLOGY SERVICES INDIA LIMITED at Cisco) <golyadav@cisco.com>
2026-03-08 01:03:39 +0530

    refactor: optimize bundle for better load speeds

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
   server: {
@@ -17,7 +18,17 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  plugins: [react(),cssInjectedByJsPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactVendor: ["react", "react-dom"],
+          router: ["react-router-dom"]
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
